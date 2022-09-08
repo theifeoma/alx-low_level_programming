3276d386863ac69b7233ea1db54caa0690638f6d
Ifeoma Okafor <if.comokafor@ymail.com>
2022-09-08 14:23:04 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,9 +8,7 @@
 
 int main(void)
 {
-	char str1[] = "Programming is like building a multilingual puzzle\n";
-
-	puts(str1);
+	puts("Programming is like building a multilingual puzzle");
 
 	return (0);
 }
