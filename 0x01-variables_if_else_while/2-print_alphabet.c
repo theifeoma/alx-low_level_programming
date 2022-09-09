a7edccea35b2d13aef9c9002eccbd7dcdd7f91fd
Ifeoma Okafor <if.comokafor@ymail.com>
2022-09-09 13:35:11 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+ * main - starts the program
+ *
+ * return - always 0
+ */
+
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz"
+	int i = 0;
+
+	while
+	(i < 26);
+
+{
+	putchar(alp[i]);
+	i++;
+}
+	putchar("\n");
+	return (0);
+}
