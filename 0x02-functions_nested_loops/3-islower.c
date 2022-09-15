c2097673dd6aaf0e4d8a74faa74e3b7b6f0502ff
Ifeoma Okafor <if.comokafor@ymail.com>
2022-09-14 23:22:27 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - prints 1 or 0 depending on input
+ * @c: is the first parameter
+ *
+ * Description: program prints all lowercase letters
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
